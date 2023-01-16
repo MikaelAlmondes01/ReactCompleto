b184ddedba93b60dcf6f3ae0b146b2b3091c5b6d
MikaelAlmondes01 <mikaelalmondes10@gmail.com>
2023-01-16 11:36:06 -0300

    wip: secao4

--- 4_CSS/css/src/App.js
+++ 4_CSS/css/src/App.js
@@ -1,9 +1,49 @@
 import "./App.css";
-
+import MyComponent from "./components/MyComponent";
+import { useState } from "react";
 function App() {
+  const n = 15;
+  const [name] = useState("Mikael");
+  const redTitle = true;
+
   return (
     <div className="App">
+      {/* css Global */}
       <h1>React com css</h1>
+      {/* css do componente */}
+      <MyComponent />
+      {/* inline css */}
+      <p
+        style={{
+          color: "magenta",
+          padding: "25px",
+          borderTop: "2px solid red",
+        }}
+      >
+        Esse elemento foi estilizado de forma inline
+      </p>
+
+      {/* inline style dinâmico */}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        Css Dinâmico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        Css Dinâmico
+      </h2>
+      <h2
+        style={
+          name === "Mikael"
+            ? { color: "magenta", backgroundColor: "#000" }
+            : null
+        }
+      >
+        Css Dinâmico
+      </h2>
+
+      {/* Classes dinâmicas no Css */}
+      <h2 className={redTitle ? "red-title" : "title"}>
+        Esse título vai ter uma classe dinâmica
+      </h2>
     </div>
   );
 }
---
+++ 4_CSS/css/src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Css do componente</h1>
+      <p>Este é o parágrafo do componente</p>
+      <p className="p_estilizado">Este é o parágrafo do componente</p>
+    </div>
+  );
+};
+
+export default MyComponent;
