3dda62681db4f5ebe315a2ed53e66bcdf0ef36ae
MikaelAlmondes01 <mikaelalmondes10@gmail.com>
2023-02-23 10:50:20 -0300

    wip: rota que não existe

--- 8_router/router/src/App.js
+++ 8_router/router/src/App.js
@@ -9,6 +9,7 @@ import About from "./pages/About";
 import Navbar from "./component/Navbar";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -24,6 +25,8 @@ function App() {
           <Route path="/products/:id" element={<Product />} />
           {/* 6 - nested route  */}
           <Route path="/products/:id/info" element={<Info />} />
+          {/* 7 - no match route */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
