cd37800d52538432ec2bddc18c22302ff0fc8163
MikaelAlmondes01 <mikaelalmondes10@gmail.com>
2023-04-20 08:09:09 -0300

    wip: Aula 123 do projeto

--- 10_Blog/miniblog/src/components/Navbar.js
+++ 10_Blog/miniblog/src/components/Navbar.js
@@ -4,21 +4,29 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <NavLink to="/">
-          Mini <span>Blog</span>
-        </NavLink>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">Sobre</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
